2748f0b91d407efb9ebe5c1c5469bd2530c8a0af
JuanseHevia <jh216@rice.edu>
2024-11-14 13:21:08 -0600

    extra functionalities to the pipeline

--- commons/pipeline.py
+++ commons/pipeline.py
@@ -1,3 +1,4 @@
+import torch
 import model 
 import retrieval
 
@@ -45,3 +46,68 @@ class Pipeline:
             self.model.clean_history()
 
         return response
+    
+    def clean_history(self):
+        """
+        Clean the history of the model.
+        """
+        self.model.clean_history()
+
+    def get_token_probs(self, query: str = None,
+                             token_idx : int = -1):
+            """
+            Get token probabilities for a given query.
+            This is commonly used for evaluation on MMLU.
+
+            Args:
+                query (str): The input query for which token probabilities are calculated.
+                token_idx (int): The index of the token for which probabilities are calculated. Default is -1.
+
+            Returns:
+                log_probs (torch.Tensor): The log probabilities of the specified token.
+
+            """
+            encoded = self.tokenizer(query, return_tensors="pt")
+            input_ids = encoded["input_ids"]
+            attn_masks = encoded["attention_mask"]
+
+            # Get the model output
+            with torch.no_grad():
+                outputs = self.model.model(input_ids=input_ids.to(self.model.device),
+                                          attention_mask=attn_masks.to(self.model.device))
+
+                logits = outputs.logits[:, token_idx, :]  # Logits at token_idx
+                log_probs = torch.nn.functional.log_softmax(logits, dim=-1)
+
+            return log_probs
+    
+    def which_next_tokens(self, token_choices: list, query: str = None,
+                              token_id_offset: int = 65, token_idx: int = -1):
+            """
+            Get the probabilities of the next tokens given a list of token choices.
+
+            Args:
+                token_choices (list): A list of token choices.
+                query (str, optional): The query string. Defaults to None.
+                token_id_offset (int, optional): The offset value for token IDs. Defaults to 65.
+                token_idx (int, optional): The index of the token. Defaults to -1.
+
+            Returns:
+                tuple: A tuple containing the index of the chosen token and a list of log probabilities for each choice.
+            """
+            token_probs = self.get_token_probs(query=query, token_idx=token_idx)
+            choice_log_probs = []
+
+            for idx in range(len(token_choices)):
+                # get index in the tokenizer vocabulary for the specific token
+                choice_token = f"{chr(token_id_offset + idx)}"  # if token_id_offset = 65, this is" A", " B", " C", " D" , ...
+                choice_ids = self.tokenizer(choice_token, return_tensors="pt")["input_ids"]
+                    
+                # Get log probability for the specific choice (A, B, C, or D)
+                choice_log_prob = token_probs[0, choice_ids[0, 0]].item()
+                choice_log_probs.append(choice_log_prob)
+
+            # compute the chosen token from the list by taking argmax on the token probs
+            pred_chosen_idx = torch.tensor(choice_log_probs).argmax().item()
+
+            return pred_chosen_idx, choice_log_probs
