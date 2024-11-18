44b7dcc31980142090c6fccfd3f4d6e7a5801841
JuanseHevia <jh216@rice.edu>
2024-11-18 09:17:44 -0500

    added MPS device support to benchmar

--- commons/benchmarks.py
+++ commons/benchmarks.py
@@ -14,6 +14,7 @@ class BenchmarkDataset:
     sample_size : int = 100
     seed : int = 42
     CHOICES = ["A", "B", "C", "D"]
+    device: str = "cpu"
 
     def __post_init__(self):
         self.dataset = load_dataset(self.name, "all")
@@ -35,7 +36,7 @@ class BenchmarkDataset:
             None
         """
         self.tokenizer = AutoTokenizer.from_pretrained(hf_path)
-        self.model = AutoModelForCausalLM.from_pretrained(hf_path)
+        self.model = AutoModelForCausalLM.from_pretrained(hf_path).to(self.device)
         self.rag = rag
         # store the choice IDs based on the chosen tokenizer
         self.choice_ids_mapping = {}
@@ -44,7 +45,7 @@ class BenchmarkDataset:
             self.choice_ids_mapping[choice_token] = choice_ids[0,0]
         
         if rag:
-            self.rag_pipeline = Retriever(pdf_path)
+            self.rag_pipeline = Retriever(pdf_path, device=self.device)
             print("RAG pipeline loaded successfully.")
 
     def _format_example(self,example):
@@ -61,7 +62,6 @@ class BenchmarkDataset:
         question = example['question']
         choices = [example[f'choices'][i] for i in range(4)]
         prompt = f''''{question}
-
             CHOICES:
             A) {choices[0]}
             B) {choices[1]}
@@ -69,7 +69,6 @@ class BenchmarkDataset:
             D) {choices[3]}
 
             Please select the correct answer by providing only the corresponding letter (A, B, C, or D).
-            ANSWER:
             '''
         if self.rag:
             context = '\n'.join(example['context'])
@@ -118,22 +117,16 @@ class BenchmarkDataset:
             # Shuffle and sample the test set
             sampled_subset = self.test_set.shuffle(seed=self.seed).select(range(self.sample_size))
 
-            # TODO: Review whether the comparison is properly done. This means, what we look for as the 
-            # predicted answer is the same as the ground truth answer. i.e. before, I had a typo and
-            # I was comparing the predicted answer to " A", " B", " C", " D" instead of "A", "B", "C", "D"
-
             for example in tqdm(sampled_subset, desc=f"Evaluating - So far gotten {correct} out of {total}"):
                 # Format the question and choices into a single prompt
                 if self.rag:
                     example['context'] = self.rag_pipeline.retrieve_documents(example['question'])
                 prompt = self._format_example(example)
                 
-                
-                
                 # Tokenize the prompt
                 inputs = self.tokenizer(prompt, return_tensors="pt")
-                input_ids = inputs["input_ids"]
-                attention_mask = inputs["attention_mask"]
+                input_ids = inputs["input_ids"].to(self.device)
+                attention_mask = inputs["attention_mask"].to(self.device)
 
                 # Compute log probabilities for each answer choice
                 choice_log_probs = []
